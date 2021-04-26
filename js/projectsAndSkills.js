a074018871d97d5beb2a25eda8a702b665442d38
danzhaas <dan.z.haas@gmail.com>
2021-04-26 00:57:34 -0400

    foundation

---
+++ js/projectsAndSkills.js
@@ -0,0 +1,247 @@
+// ██████╗ ██████╗  ██████╗      ██╗███████╗ ██████╗████████╗███████╗
+// ██╔══██╗██╔══██╗██╔═══██╗     ██║██╔════╝██╔════╝╚══██╔══╝██╔════╝
+// ██████╔╝██████╔╝██║   ██║     ██║█████╗  ██║        ██║   ███████╗
+// ██╔═══╝ ██╔══██╗██║   ██║██   ██║██╔══╝  ██║        ██║   ╚════██║
+// ██║     ██║  ██║╚██████╔╝╚█████╔╝███████╗╚██████╗   ██║   ███████║
+// ╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚══════╝ ╚═════╝   ╚═╝   ╚══════╝
+
+
+//Alien Vs Editor
+const ave = {
+    name:"Alien Vs Editor",
+    id:"ave",
+    thumbnailSrc:"images/AvE-tile-500.jpg",
+    imageSrc:"images/AvE-landscape.jpg",
+    devIcons:[
+        "Node.js","Express","MongoDB","React", "React Router","Javascript","Sass"
+    ],
+    desc:'Rich text editor my 3-person team built in one week for a Hackathon challenge.  Features cloud file management.',
+    button1: {
+        text:"Live Version",
+        href:"https://our-mintbean-team.github.io/texteditor-mintbean/"
+    },
+    button2:{
+        text:"See Code",
+        href:"https://github.com/our-mintbean-team/texteditor-mintbean"
+    }
+}
+
+
+//Dev Connector
+const dev = {
+    name:"Developer Web",
+    id:"dev",
+    thumbnailSrc:"images/dev-tile-500.jpg",
+    imageSrc:"images/dev-connector-landscape.jpg",
+    devIcons:[
+        "Node.js","Express","MongoDB","React", "Redux", "React Router","Javascript"
+    ],
+    desc:'Social network full stack web application.  Features chat functionality, use of async/await, and Redux.',
+    button1: {
+        text:"Live Version",
+        href:"https://dev-connector-dzh.herokuapp.com/"
+    },
+    button2:{
+        text:"See Code",
+        href:"http://github.com/danzhaas/dev-connector"
+    }
+}
+
+
+//Barkr
+const barkr = {
+    name:"Barkr",
+    id:"barkr",
+    thumbnailSrc:"images/barkr-tile-500.jpg",
+    imageSrc:"images/barkr-landscape.jpg",
+    devIcons:[
+        "React","React Router","Node.js","Express","Javascript","Bootstrap","HTML","CSS"
+    ],
+    desc:'My "pet project": a companion to your companion. Share your best friend with the world on this dog social network.',
+    button1: {
+        text:"Live Version",
+        href:"https://danzhaas.github.io/barkr-react/"
+    },
+    button2:{
+        text:"See Code",
+        href:"http://github.com/danzhaas/barkr-fullstack"
+    }
+}
+
+
+//distractionTimer
+const distractionTimer = {
+    name:"Distraction Timer",
+    id:"timer",
+    thumbnailSrc:"images/timer-tile-500.jpg",
+    imageSrc:"images/timer-landscape.jpg",
+    devIcons:[
+        "Gatsby","React","React Google Charts","Javascript","Bootstrap","HTML","CSS","Sass"
+    ],
+    desc:'A productivity tool to measure and chart how much time you are distracted during a task.',
+    button1: {
+        text:"Live Version",
+        href:"https://danzhaas.github.io/Focus-Suite",
+        attribute: 'rel="popup console 600 375"'
+    },
+    button2:{
+        text:"See Code",
+        href:"https://github.com/danzhaas/danzhaas.github.io/tree/source/project-adhd/distraction-timer"
+    }
+}
+
+
+//Killifish Classic
+const killi = {
+    name:"Killifish Classic",
+    id:"killi",
+    thumbnailSrc:"images/killi-tile-500.jpg",
+    imageSrc:"images/killi-tile-500.jpg",
+    devIcons:[
+        "jQuery","Javascript","HTML","CSS","less"
+    ],
+    desc:'An event page for an aquarium convention. The design incorporates the palette of beautifully bright colors of these fish.',
+    button1: {
+        text:"Live Version",
+        href:"https://danzhaas.github.io/killifish-classic/"
+    },
+    button2:{
+        text:"See Code",
+        href:"http://github.com/danzhaas/danzhaas.github.io/tree/master/killifish-classic"
+    }
+}
+
+
+//Lorem Ipsum Cancer Studies
+const LICS = {
+    name:"Lorem Ipsum Cancer Studies",
+    id:"LICS",
+    thumbnailSrc:"images/LICS-tile-500.jpg",
+    imageSrc:"images/LICS-landscape.jpg",
+    devIcons:[
+        "jQuery","Bootstrap","HTML","CSS"
+    ],
+    desc:'A university medical research study site. UI designed around user stories for participants, scientists, and sponsors.',
+    button1: {
+        text:"Live Version",
+        href:"https://danzhaas.github.io/lorem-ipsum-cancer-studies/index.html"
+    },
+    button2:{
+        text:"See Code",
+        href:"https://github.com/danzhaas/danzhaas.github.io/tree/master/lorem-ipsum-cancer-studies"
+    }
+}
+
+
+// My Portfolio
+const portfolio = {
+    name:"My Portfolio",
+    id:"portfolio",
+    thumbnailSrc:"images/portfolio-tile-500.jpg",
+    imageSrc:"images/portfolio-landscape.jpg",
+    devIcons:[
+        "Node.js","Express","MongoDB","jQuery","Javascript","Bootstrap","Sass","CSS","HTML"
+    ],
+    desc:"You're looking at it.  Visit my Trello board to see how I used a Kanban board to plan and create this project.",
+    button1: {
+        text:"Trello Board",
+        href:"https://trello.com/b/pXxa9X5h/danzhaasgithubio"
+    },
+    button2:{
+        text:"See Code",
+        href:"https://github.com/danzhaas/danzhaas.github.io/tree/master/index.html"
+    }
+}
+
+
+// List of Projects
+const myProjects = [dev, distractionTimer, barkr, LICS, portfolio, ave];
+
+
+// Development Icon Library
+const allDevIcons = [
+    gatsby={
+        name:"Gatsby",
+        svg:"images/icons/gatsby.svg",
+    },
+    jQuery={
+        name:"jQuery",
+        svg:"images/icons/jquery.svg",
+    },
+    react={
+        name:"React",
+        svg:"images/icons/react.svg",
+    },
+    redux = {
+        name:"Redux",
+        svg:"images/icons/redux.svg"
+    },
+    reactrouter={
+        name:"React Router",
+        svg:"images/icons/reactrouter.svg"
+    },
+    reactstrap={
+        name:"Reactstrap",
+        svg:"images/icons/reactstrap.png"
+    },
+    reactgooglecharts={
+        name:"React Google Charts",
+        svg:"images/icons/reactgooglecharts.png"
+    },
+    javascript={
+        name:"Javascript",
+        svg:"images/icons/js.svg",
+    },
+    bootstrap={
+        name:"Bootstrap",
+        svg:"images/icons/bootstrap.svg",
+    },
+    HTMLicon={
+        name:"HTML",
+        svg:"images/icons/html.svg",
+    },
+    CSSicon={
+        name:"CSS",
+        svg:"images/icons/css.svg",
+    },
+    nodeJS={
+        name:"Node.js",
+        svg:"images/icons/nodejs.svg",
+    },
+    express={
+        name:"Express",
+        svg:"images/icons/express.svg",
+    },
+    mongodb={
+        name:"MongoDB",
+        svg:"images/icons/mongodb.svg",
+    },
+    sass={
+        name:"Sass",
+        svg:"images/icons/sass.svg"
+    },
+    less={
+        name:"less",
+        svg:"images/icons/less.svg"
+    }
+]
+
+
+// ███████╗██╗  ██╗██╗██╗     ██╗     ███████╗
+// ██╔════╝██║ ██╔╝██║██║     ██║     ██╔════╝
+// ███████╗█████╔╝ ██║██║     ██║     ███████╗
+// ╚════██║██╔═██╗ ██║██║     ██║     ╚════██║
+// ███████║██║  ██╗██║███████╗███████╗███████║
+// ╚══════╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚══════╝
+
+
+// Development Skills
+const devBadges = "React, Node.js, Express, MongoDB, Gatsby, Redux, React Router, D3, JQuery, Javascript, Bootstrap, LESS, SASS, CSS, HTML, NPM, React Native, Github, Browser Developer Tools, XML";
+// Design Skills
+const designBadges = "Photoshop, GIMP, Adobe Illustrator";
+// Databases
+const databaseBadges = "MySQL, MS Access, MongoDB, MongoDBCompass";
+// Languages
+const languagesBadges = "Visual Basic for Applications, Javascript";
+// OS
+const osBadges = "Windows, Linux, Mac";
---
+++ js/script.js
@@ -0,0 +1,184 @@
+//  ██████╗ ██╗      ██████╗ ██████╗  █████╗ ██╗     
+// ██╔════╝ ██║     ██╔═══██╗██╔══██╗██╔══██╗██║     
+// ██║  ███╗██║     ██║   ██║██████╔╝███████║██║     
+// ██║   ██║██║     ██║   ██║██╔══██╗██╔══██║██║     
+// ╚██████╔╝███████╗╚██████╔╝██████╔╝██║  ██║███████╗
+//  ╚═════╝ ╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+
+// SCROLLSPY
+$('body').scrollspy({ target: '#navbar-collapse' });
+
+
+// ██████╗ ██████╗  ██████╗      ██╗███████╗ ██████╗████████╗███████╗
+// ██╔══██╗██╔══██╗██╔═══██╗     ██║██╔════╝██╔════╝╚══██╔══╝██╔════╝
+// ██████╔╝██████╔╝██║   ██║     ██║█████╗  ██║        ██║   ███████╗
+// ██╔═══╝ ██╔══██╗██║   ██║██   ██║██╔══╝  ██║        ██║   ╚════██║
+// ██║     ██║  ██║╚██████╔╝╚█████╔╝███████╗╚██████╗   ██║   ███████║
+// ╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚══════╝ ╚═════╝   ╚═╝   ╚══════╝
+
+
+// GENERATE CARDS
+var cardTemplate, iconsTemplate;
+
+function populateTemplate(project) {
+
+    cardTemplate = `
+        <div id="${project.id}-card" class="project card mb-3">
+            <div class="row g-0">
+
+                <div class="col-md-8">
+                    <div class="front zoom">
+                        <a href='${project.button1.href}' target='_blank'>
+                            <img class="card-img d-md-none" src="${project.thumbnailSrc}"
+                                alt="${project.altText}" />
+                            <img class="d-none d-md-block card-img rounded-0" src="${project.imageSrc}"
+                                alt="${project.altText}" />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card-body">
+                        <h2 class="card-title m-0">${project.name}</h2>
+                        <div id="${project.id}-icons">
+                        </div>
+                        <p class="card-text m-0">${project.desc}</p>
+                        <div class="d-flex flex-row justify-content-around">
+                            <a role="button" class="btn btn-sm" href="${project.button1.href}" target="_blank" ${project.button1.attribute} >
+                                ${project.button1.text}
+                            </a>
+                            <a role="button" class="btn btn-sm" href="${project.button2.href}" target="_blank">
+                                ${project.button2.text}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    `
+}
+
+function generateIcons(icons) {
+    iconsTemplate="";
+    icons.forEach(function(iconName) {
+        var iconRef=allDevIcons.filter((icon) => icon.name===iconName);
+        if (iconRef.length === 1) {
+            iconsTemplate=iconsTemplate.concat(`<img class="dev-icon" src="${iconRef[0].svg}" data-toggle="tooltip" title="${iconRef[0].name}"></img>`)
+        } else {
+            iconsTemplate=iconsTemplate.concat(`<span class="dev-icon badge badge-info">${iconName}</span>`)
+        }
+    });
+}
+
+function buildCard(project) {
+    var cardContainer = document.createElement("div");  
+    cardContainer.classList="card-container col-12";  
+    cardContainer.innerHTML = cardTemplate;     
+    document.getElementById("cards").appendChild(cardContainer);  
+    var iconTarget = $(`#${project.id}-icons`); 
+    iconTarget[0].innerHTML = iconsTemplate;  
+}
+
+function generateCard(project) {
+    populateTemplate(project);
+    generateIcons(project.devIcons);
+    buildCard(project);
+}
+
+myProjects.forEach(generateCard);
+
+// TOOLTIPS
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+//  █████╗ ██████╗  ██████╗ ██╗   ██╗████████╗
+// ██╔══██╗██╔══██╗██╔═══██╗██║   ██║╚══██╔══╝
+// ███████║██████╔╝██║   ██║██║   ██║   ██║   
+// ██╔══██║██╔══██╗██║   ██║██║   ██║   ██║   
+// ██║  ██║██████╔╝╚██████╔╝╚██████╔╝   ██║   
+// ╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚═════╝    ╚═╝   
+
+
+//RESUME MODAL
+document.getElementById("resume-icon").addEventListener("click", () => $('#resumeModal').modal('toggle'));
+
+
+//GENERATE BADGES
+function generateBadges(listString, badgeColor, nothing) {
+    const badgeArray = listString.split(", ").map(text => "<div class='badge-container zoom'><span class='badge badge-"+badgeColor+" mr-1'>" +text + "</span></div>");
+    switch (listString) {
+        case devBadges:
+            targetSelector = ".dev-badges";
+            break;
+        case designBadges:
+            targetSelector = ".design-badges";
+            break;
+        case databaseBadges:
+            targetSelector = ".database-badges";
+            break;
+        case languagesBadges:
+            targetSelector = ".languages-badges";
+            break;
+        case osBadges:
+            targetSelector = ".os-badges";
+    }
+    $(targetSelector).append(badgeArray)
+}
+
+generateBadges(devBadges, "primary");
+generateBadges(designBadges, "info");
+generateBadges(databaseBadges, "success");
+generateBadges(languagesBadges, "secondary");
+generateBadges(osBadges, "dark")
+
+document.querySelectorAll(".badge").forEach(badge => badge.addEventListener("mouseover", (event) => {
+    badge.classList.toggle("barrel-roll");
+}))
+
+
+//  ██████╗ ██████╗ ███╗   ██╗████████╗ █████╗  ██████╗████████╗
+// ██╔════╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██╔════╝╚══██╔══╝
+// ██║     ██║   ██║██╔██╗ ██║   ██║   ███████║██║        ██║   
+// ██║     ██║   ██║██║╚██╗██║   ██║   ██╔══██║██║        ██║   
+// ╚██████╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║╚██████╗   ██║   
+//  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝   ╚═╝   
+
+
+function sendForm (formSize) {
+    document.querySelectorAll(".submit-btn").forEach(node => 
+        node.value='Sending...'
+    );
+    const formValues = {
+        "name":document.querySelector('#name-'+formSize).value,
+        "email":document.querySelector('#email-'+formSize).value,
+        "formMessage":document.querySelector('#formMessage-'+formSize).value
+    };
+    $.ajax({
+        url:'https://danzhaas-github-io-back-end.herokuapp.com/api/messages/',
+        type:"POST",
+        data:formValues,
+        success: function() {
+            alert("Message received, thank you. I will get back to you within 3 business days.");
+            document.querySelectorAll(".submit-btn").forEach(node => node.value='Message Sent');
+        },
+        error: function() {
+            alert("Server error.  Try again later or email me directly at danzhaas@gmail.com .");
+            document.querySelectorAll(".submit-btn").forEach(node => node.value='Send Message')
+        }
+    });
+};
+
+$('#form-large').submit(function(e) {
+    e.preventDefault(); // prevents form from submitting
+    sendForm("l");
+})
+
+
+$('#form-small').submit(function(e) {
+    e.preventDefault(); // prevents form from submitting
+    sendForm("s");
+})
